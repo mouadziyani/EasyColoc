6d9756f498473d5dea241695127a1c32cc4d3c6f
MOEUAED <ziyanimouad@gmail.com>
2026-03-02 10:01:21 +0000

    chore: update app/Models/Expense.php

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -7,6 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class Expense extends Model
 {
     protected $fillable = [
-        'name'
+        'title',
+        'amount',
+        'category_id',
+        'paid_by',
+        'date',
+        'colocation_id'
     ];
-}
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'paid_by');
+    }
+    }
