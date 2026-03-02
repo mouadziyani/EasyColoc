e3ef185bca10a81f9bceb6752a73c1d4a1f8e3d7
MOEUAED <ziyanimouad@gmail.com>
2026-03-02 10:01:22 +0000

    chore: update app/Models/Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
-}
+        protected $fillable = [
+            'colocation_id',
+            'payer_id',
+            'receiver_id',
+            'amount',
+            'paid_at',
+        ];
+
+        public function colocation()
+        {
+            return $this->belongsTo(Colocation::class);
+        }
+
+        public function payer()
+        {
+            return $this->belongsTo(User::class, 'payer_id');
+        }
+
+        public function receiver()
+        {
+            return $this->belongsTo(User::class, 'receiver_id');
+        }
+        }
