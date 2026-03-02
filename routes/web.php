6ad4ed38e425bd8fa8805baafd559614d6407c02
MOEUAED <ziyanimouad@gmail.com>
2026-03-02 10:01:24 +0000

    chore: update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,34 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Colocation;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\ColocationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $user = Auth::user();
+    $owned = $user->colocations;
+    $member = $user->memberships->map->colocation;
+    $colocations = $owned->merge($member);
+
+    return view('dashboard', compact('colocations'));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Routes pour les colocations
+    Route::resource('colocations', ColocationController::class);
+
+    // Routes pour les dépenses
+    Route::resource('colocations.expenses', ExpenseController::class);
 });
 
 require __DIR__.'/auth.php';
