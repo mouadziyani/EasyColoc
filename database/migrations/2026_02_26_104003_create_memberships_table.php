e129ba898d91ae5ec80f36fc0a81bf0280db3686
MOEUAED <ziyanimouad@gmail.com>
2026-03-02 14:11:40 +0000

    chore: update database/migrations/2026_02_26_104003_create_memberships_table.php

--- database/migrations/2026_02_26_104003_create_memberships_table.php
+++ database/migrations/2026_02_26_104003_create_memberships_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('colocation_id')->constrained()->cascadeOnDelete();
             $table->string('role')->default('member');
+            $table->string('status')->default('active');
             $table->timestamp('joined_at')->nullable();
+            $table->timestamp('left_at')->nullable();
             $table->timestamps();
         });
     }
