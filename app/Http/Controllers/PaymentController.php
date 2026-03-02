e6f60bfd36b46fcdd4261a57d87821c399fc6d49
MOEUAED <ziyanimouad@gmail.com>
2026-03-02 14:59:37 +0000

    chore: update app/Http/Controllers/PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,21 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Payment;
 use Illuminate\Http\Request;
+use App\Models\Colocation;
 use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
-    public function store(Request $request)
+    public function store(Request $request, Colocation $colocation)
     {
         $validated = $request->validate([
-            'payer_id' => 'required|exists:users,id',
+            'sender_id' => 'required|exists:users,id',
             'receiver_id' => 'required|exists:users,id',
-            'amount' => 'required|numeric|min:0.01',
-            'colocation_id' => 'required|exists:colocations,id',
+            'amount' => 'required|numeric|min:0',
         ]);
 
-        Payment::create($validated);
+        Payment::create([
+            'colocation_id' => $colocation->id,
+            'sender_id' => $validated['sender_id'],
+            'receiver_id' => $validated['receiver_id'],
+            'amount' => $validated['amount'],
+        ]);
 
-        return back()->with('success', 'Paiement enregistré avec succès.');
+        return redirect()->back()->with('success', 'Paiement enregistré!');
     }
 }
